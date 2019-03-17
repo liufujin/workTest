4d553d3c0060e5eba9b02bf7383d251f486240a1
zhangsan <zhangsan@qq.com>
2019-03-17 13:11:17 +0800

    修改java 添加一行输出语句！！

--- Demo01/src/main/java/com/work/java/JavaDemo01.java
+++ Demo01/src/main/java/com/work/java/JavaDemo01.java
@@ -3,6 +3,7 @@ package com.work.java;
 public class JavaDemo01 {
     public static void main(String[] args) {
         System.out.println("this is a java class!!");
+        System.out.println("222222");
 
     }
 }
